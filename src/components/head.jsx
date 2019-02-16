5c48c73f4301b7add18c052391d4d55ab3534f30
Raj Sagar <therajsagar@gmail.com>
2019-02-16 21:04:05 +0530

    First Commit

---
+++ src/components/head.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/head.css';
+
+const Head = () => <div className='head'>
+<input type='text' placeholder='Search for a User'/>
+</div>
+
+
+
+export default Head;
