6fb17adc8fb55aa95dffa0f1ee02f2e760d41a56
Raj Sagar <therajsagar@gmail.com>
2019-03-09 18:41:59 +0530

    Updates

---
+++ precache-manifest.f4efad54c4f4b7b989611a002ce8d5e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aca5bba49fe672a4fa2a",
+    "url": "/github-user-search/static/js/runtime~main.aca5bba4.js"
+  },
+  {
+    "revision": "b190338d3d7081530871",
+    "url": "/github-user-search/static/js/main.b190338d.chunk.js"
+  },
+  {
+    "revision": "6d122e3808195037b8db",
+    "url": "/github-user-search/static/js/2.6d122e38.chunk.js"
+  },
+  {
+    "revision": "b190338d3d7081530871",
+    "url": "/github-user-search/static/css/main.4ea32b6c.chunk.css"
+  },
+  {
+    "revision": "61224649b43f3a444708726c72dfea0f",
+    "url": "/github-user-search/index.html"
+  }
+];
